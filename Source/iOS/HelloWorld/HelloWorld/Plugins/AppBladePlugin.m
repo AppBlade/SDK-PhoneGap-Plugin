df85c44c2d652294f5c5ebbe9b696411ca9b84a5
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-03-14 12:11:52 -0400

    Adding new Plugin calls in /Plugins/AppBladePlugin.m and AppBlade.js

--- Source/iOS/HelloWorld/HelloWorld/Plugins/AppBladePlugin.m
+++ Source/iOS/HelloWorld/HelloWorld/Plugins/AppBladePlugin.m
@@ -36,20 +36,66 @@ enum {
 
 - (void)catchAndReportCrashes:(NSMutableArray *)args withDict:(NSMutableDictionary *)options
 {
-    NSLog(@"Catch and Report Crashes");
+    NSLog(@"Catch and Report Crashes via Plugin");
     [[AppBlade sharedManager] catchAndReportCrashes];
 }
 
 - (void)checkAuthentication:(NSMutableArray *)args withDict:(NSMutableDictionary *)options
 {
-    NSLog(@"Check approval");
+    NSLog(@"Check approval via Plugin");
     [[AppBlade sharedManager] checkApproval];
 }
 
 - (void)allowFeedbackReporting:(NSMutableArray *)args withDict:(NSMutableDictionary *)options
 {
-    NSLog(@"Allow Feedback");
-    [[AppBlade sharedManager] allowFeedbackReporting];
+    NSLog(@"Allow Feedback via Plugin");
+    if(null != args && [[args objectAtIndex:0] isKindOfClass:[NSString class]])
+    {
+        NSLog(@"Custom Feedback via Plugin");
+        if([((NSString *)[args objectAtIndex:0]) isEqualToString:@"Custom"])
+        [[AppBlade sharedManager] setupCustomFeedbackReporting];
+    }
+    else
+    {
+        [[AppBlade sharedManager] allowFeedbackReporting];
+    }
 }
+
+- (void)showFeedbackDialog:(NSMutableArray *)args withDict:(NSMutableDictionary *)options
+{
+    NSLog(@"Show Feedback via Plugin");
+    [[AppBlade sharedManager] showFeedbackDialogue];
+}
+
+
+
+- (void)startSession:(NSMutableArray *)args withDict:(NSMutableDictionary *)options
+{
+    NSLog(@"Start Session via Plugin");
+    [[AppBlade sharedManager] startSession];
+}
+
+- (void)endSession:(NSMutableArray *)args withDict:(NSMutableDictionary *)options
+{
+    NSLog(@"End Session via Plugin");
+    [[AppBlade sharedManager] endSession];
+}
+
+
+- (void)setCustomParameter:(NSMutableArray *)args withDict:(NSMutableDictionary *)options
+{
+    NSLog(@"Setting a Custom Parameter via Plugin");
+    NSArray* keyAndVal = [args objectAtIndex:0];
+    NSString* key = [keyAndVal objectAtIndex:0];
+    NSString* key = [keyAndVal objectAtIndex:0];
+
+    [AppBlade sharedManager] setCustomParam:<#(id)#> withValue:<#(NSString *)#>
+}
+
+- (void)clearCustomParameters:(NSMutableArray *)args withDict:(NSMutableDictionary *)options
+{
+    NSLog(@"Clearing all parameters via Plugin");
+}
+
 
 @end
--- Source/iOS/HelloWorld/www/AppBlade.js
+++ Source/iOS/HelloWorld/www/AppBlade.js
@@ -15,18 +15,53 @@ var AppBlade = function(){};
 AppBlade.prototype.setupAppBlade = function(project, token, secret, timestamp) {
     cordova.exec("AppBlade.setupAppBlade", [project, token, secret, timestamp]);
 };
-    
-AppBlade.prototype.catchAndReportCrashes = function() {
-    cordova.exec("AppBlade.catchAndReportCrashes");
+
+
+AppBlade.prototype.checkForAnonymousUpdates = function() {
+    cordova.exec("AppBlade.checkforUpdates");
 };
     
 AppBlade.prototype.checkAuthentication = function() {
     cordova.exec("AppBlade.checkAuthentication");
 };
-    
+
+AppBlade.prototype.catchAndReportCrashes = function() {
+    cordova.exec("AppBlade.catchAndReportCrashes");
+};
+
+
 AppBlade.prototype.allowFeedbackReporting = function() {
     cordova.exec("AppBlade.allowFeedbackReporting");
 };
+AppBlade.prototype.showFeedbackDialog = function() {
+    cordova.exec("AppBlade.showFeedbackDialog");
+};
+
+
+AppBlade.prototype.startSession = function() {
+    cordova.exec("AppBlade.startSession");
+};
+
+AppBlade.prototype.endSession = function() {
+    cordova.exec("AppBlade.endSession");
+};
+
+
+
+AppBlade.prototype.setCustomParameter = function(key, value) {
+    cordova.exec("AppBlade.setCustomParameter", [key, value]);
+};
+
+AppBlade.prototype.setAllCustomParameters = function(dictionary) {
+    cordova.exec("AppBlade.setCustomParameters", dictionary);
+};
+
+AppBlade.prototype.clearCustomParameterss = function() {
+    cordova.exec("AppBlade.clearCustomParameters");
+};
+
+
+
 
 // --------------------------------------------------------
 
