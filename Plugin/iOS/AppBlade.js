22960120b333b7206623354b1e7f4c6e7579e21b
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-03-27 17:33:03 -0400

    Updating the iOS plugin first for PhoneGap 2.5.0

--- Plugin/iOS/AppBlade.js
+++ Plugin/iOS/AppBlade.js
@@ -1,40 +1,95 @@
 /**
  * AppBlade.js
- *  
+ *
  * Phonegap AppBlade Instance plugin
  * Copyright (c) AppBlade 2012
  *
  */
-  
-// --------------------------------------------------------
-  
-var AppBlade = function(){};
 
 // --------------------------------------------------------
-  
-AppBlade.prototype.setupAppBlade = function(project, token, secret, timestamp) {
-    cordova.exec("AppBlade.setupAppBlade", [project, token, secret, timestamp]);
+
+var AppBlade = function(){
+        this.serviceName = "AppBlade";
 };
-    
-AppBlade.prototype.catchAndReportCrashes = function() {
-    cordova.exec("AppBlade.catchAndReportCrashes");
+
+// --------------------------------------------------------
+
+//AppBlade Registration
+AppBlade.prototype.setupAppBlade = function(project, token, secret, timestamp, successFunction, failFunction) {
+    return cordova.exec(successFunction, failFunction, this.serviceName, "AppBlade.setupAppBlade", [project, token, secret, timestamp], successFunction);
 };
-    
-AppBlade.prototype.checkAuthentication = function() {
-    cordova.exec("AppBlade.checkAuthentication");
+
+//Update Checks
+AppBlade.prototype.checkForAnonymousUpdates = function(successFunction, failFunction) {
+    return cordova.exec(successFunction, failFunction, this.serviceName, "AppBlade.checkforUpdates", []);
 };
-    
-AppBlade.prototype.allowFeedbackReporting = function() {
-    cordova.exec("AppBlade.allowFeedbackReporting");
+
+//Authentication Checks
+AppBlade.prototype.checkAuthentication = function(successFunction, failFunction) {
+    return cordova.exec(successFunction, failFunction, this.serviceName, "AppBlade.checkAuthentication", []);
 };
+
+//Crash Reporting
+AppBlade.prototype.catchAndReportCrashes = function(successFunction, failFunction) {
+    return cordova.exec(successFunction, failFunction, this.serviceName, "AppBlade.catchAndReportCrashes", []);
+};
+
+//Feedback Reporting
+AppBlade.prototype.allowFeedbackReporting = function(successFunction, failFunction) {
+   return cordova.exec(successFunction, failFunction, this.serviceName, "AppBlade.allowFeedbackReporting", []);
+};
+AppBlade.prototype.showFeedbackDialog = function(successFunction, failFunction) {
+    return cordova.exec(successFunction, failFunction, this.serviceName, "AppBlade.showFeedbackDialog", []);
+};
+
+//Session Logging
+AppBlade.prototype.startSession = function(successFunction, failFunction) {
+    return cordova.exec(successFunction, failFunction, this.serviceName, "AppBlade.startSession", []);
+};
+
+AppBlade.prototype.endSession = function(successFunction, failFunction) {
+    return cordova.exec(successFunction, failFunction, this.serviceName, "AppBlade.endSession", []);
+};
+
+
+//Custom Parameters
+AppBlade.prototype.setCustomParameter = function(key, value, successFunction, failFunction) {
+    return cordova.exec(successFunction, failFunction, this.serviceName, "AppBlade.setCustomParameter", [key, value]);
+};
+
+AppBlade.prototype.setAllCustomParameters = function(dictionary, successFunction, failFunction) {
+    return cordova.exec(successFunction, failFunction, this.serviceName, "AppBlade.setCustomParameters", [dictionary]);
+};
+
+AppBlade.prototype.clearCustomParameters = function(successFunction, failFunction) {
+    return cordova.exec(successFunction, failFunction, this.serviceName, "AppBlade.clearCustomParameters", []);
+};
+
+// --------------------------------------------------------
+
+
+AppBlade.install = = function(){
+    if (typeof window.plugins == "undefined") window.plugins = {};
+    if (typeof window.plugins.AppBlade == "undefined") window.plugins.AppBlade = new AppBlade();
+    return window.plugins.AppBlade;
+};
+
+if(!window.plugins) {
+    window.plugins = {};
+}
+
+if (!window.plugins.AppBlade) {
+    window.plugins.AppBlade = new AppBlade();
+}
+
 
 // --------------------------------------------------------
 
 cordova.addConstructor(function() {
-    if (!window.Cordova) {
-    window.Cordova = cordova;
-    };
+                       if (!window.Cordova) {
+                       window.Cordova = cordova;
+                       };
                        
-    if(!window.plugins) window.plugins = {};
-    window.plugins.appBlade = new AppBlade();
-});
+                       if(!window.plugins) window.plugins = {};
+                       window.plugins.appBlade = new AppBlade();
+                       });
--- Plugin/iOS/AppBladePlugin.h
+++ Plugin/iOS/AppBladePlugin.h
@@ -10,10 +10,28 @@
 
 @interface AppBladePlugin : CDVPlugin
 
-- (void)setupAppBlade:(NSMutableArray*)args withDict:(NSMutableDictionary*)options;
+- (void)setupAppBlade:(CDVInvokedUrlCommand*)command;
 
-- (void)catchAndReportCrashes:(NSMutableArray*)args withDict:(NSMutableDictionary*)options;
-- (void)allowFeedbackReporting:(NSMutableArray*)args withDict:(NSMutableDictionary*)options;
-- (void)checkAuthentication:(NSMutableArray*)args withDict:(NSMutableDictionary*)options;
+- (void)checkAuthentication:(CDVInvokedUrlCommand*)command;
+
+- (void)checkForUpdates:(CDVInvokedUrlCommand*)command;
+
+- (void)catchAndReportCrashes:(CDVInvokedUrlCommand*)command;
+
+- (void)allowFeedbackReporting:(CDVInvokedUrlCommand*)command;
+- (void)showFeedbackDialog:(CDVInvokedUrlCommand*)command;
+
+- (void)startSession:(CDVInvokedUrlCommand*)command;
+- (void)endSession:(CDVInvokedUrlCommand*)command;
+
+- (void)setCustomParameter:(CDVInvokedUrlCommand*)command;
+- (void)clearCustomParameters:(CDVInvokedUrlCommand*)command;
+
+//DEPRECATED METHODS. WILL BE REMOVED IN THE IMMEDIATE FUTURE
+- (void)setupAppBlade:(NSMutableArray*)args withDict:(NSMutableDictionary*)options __attribute__((deprecated));
+- (void)catchAndReportCrashes:(NSMutableArray*)args withDict:(NSMutableDictionary*)options __attribute__((deprecated));
+- (void)allowFeedbackReporting:(NSMutableArray*)args withDict:(NSMutableDictionary*)options __attribute__((deprecated));
+- (void)checkAuthentication:(NSMutableArray*)args withDict:(NSMutableDictionary*)options __attribute__((deprecated));
+- (void)setCustomParameter:(NSMutableArray *)args withDict:(NSMutableDictionary *)options __attribute__((deprecated));
 
 @end
