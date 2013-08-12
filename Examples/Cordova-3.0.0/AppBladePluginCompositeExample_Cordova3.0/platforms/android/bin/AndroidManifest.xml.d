8feeffbdeed44c743c5c8e30713923e4c4dc1f49
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-08-12 12:03:51 -0400

    pulling out cordova 3.0 to its own branch for now

--- Examples/Cordova-3.0.0/AppBladePluginCompositeExample_Cordova3.0/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/andrewtremblay/Development/AppBlade-PhoneGap/Examples/Cordova-3.0.0/AppBladePluginCompositeExample_Cordova3.0/platforms/android/bin/AndroidManifest.xml : \
-/Users/andrewtremblay/Development/AppBlade-PhoneGap/Examples/Cordova-3.0.0/AppBladePluginCompositeExample_Cordova3.0/platforms/android/AndroidManifest.xml \
--- Examples/Cordova-3.0.0/AppBladePluginCompositeExample_Cordova3.0/platforms/android/bin/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/andrewtremblay/Development/AppBlade-PhoneGap/Examples/Cordova-3.0.0/AppBladePluginCompositeExample_Cordova3.0/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
-/Users/andrewtremblay/Development/AppBlade-PhoneGap/Examples/Cordova-3.0.0/AppBladePluginCompositeExample_Cordova3.0/platforms/android/bin/HelloWorld.ap_ \
-/Users/andrewtremblay/Development/AppBlade-PhoneGap/Examples/Cordova-3.0.0/AppBladePluginCompositeExample_Cordova3.0/platforms/android/bin/classes.dex \
-/Users/andrewtremblay/Development/AppBlade-PhoneGap/Examples/Cordova-3.0.0/AppBladePluginCompositeExample_Cordova3.0/platforms/android/libs/cordova-3.0.0.jar \
